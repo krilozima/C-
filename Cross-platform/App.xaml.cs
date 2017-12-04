a701b8c5666cc76c50b5aecdade73a0345f9f81d
krilozima <markeu.cyril@gmail.com>
2017-12-04 21:53:18 +0800

    Add files via upload

---
+++ Cross-platform/App.xaml.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Xamarin.Forms;
+
+namespace LUBA
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+            var image = new Image { Source = "waterfront.jpg" };
+
+            MainPage = new Data_Entry.MainPage();
+        }
+
+        protected override void OnStart()
+        {
+            // Handle when your app starts
+        }
+
+        protected override void OnSleep()
+        {
+            // Handle when your app sleeps
+        }
+
+        protected override void OnResume()
+        {
+            // Handle when your app resumes
+        }
+    }
+}
